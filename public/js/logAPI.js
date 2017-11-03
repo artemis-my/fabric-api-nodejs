dc9f3b9f2e6aee8541ee3fd6dcf139038ab4760b
mengyu_zhu <jsmengyu_zhu@163.com>
2017-11-03 19:41:32 +0800

    page has problem

--- public/js/logAPI.js
+++ public/js/logAPI.js
@@ -3,6 +3,31 @@
 		//sessionStorage.token="<%=token%>";
 		//sessionStorage.user="<%=user%>";
 		//getAllLog("queryLogsByUser",sessionStorage.username);
+var options={
+                bootstrapMajorVersion:3,    //版本
+                currentPage:1,    //当前页数
+                numberOfPages:5,    //最多显示Page页
+                totalPages:10,    //所有数据可以显示的页数
+		itemTexts: function (type, page, current) {
+		    switch (type) {
+		        case "first":
+		            return "首页";
+		        case "prev":
+		            return "上一页";
+		        case "next":
+		            return "下一页";
+		        case "last":
+		            return "末页";
+		        case "page":
+		            return page;
+           	 }
+        },
+                onPageClicked:function(e,originalEvent,type,page){
+				getAll(page);
+
+                }
+            }
+            $("#page").bootstrapPaginator(options);
 		getAll(1);
 		$("#sendbtn").click(function(){
 			$("#front").hide();
