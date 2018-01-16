6db5900fa09cde282bcb315e8a0d4bd6a81c07dc
mengyu_zhu <jsmengyu_zhu@163.com>
2018-01-16 15:44:31 +0800

    change fabricusers.sql

--- db/fabricUsers.sql
+++ db/fabricUsers.sql
@@ -1,14 +1,14 @@
 use fabricexplorer;
-drop table fabricusers;
+drop table if exists fabricusers;
 create table fabricusers
 (
     userid int primary key auto_increment,
     username varchar(50) not null,
     userpassword varchar(50) not null,
     phonenumber varchar(20) not null,
-    org varchar(100) not null,
-    balance double 
+    org varchar(100) not null
 );
+drop table if exists logsinfo;
 create table logsinfo
 (
 	logid int primary key auto_increment,
